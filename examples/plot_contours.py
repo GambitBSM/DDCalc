f9d8cecd1e5e23fcf5b2991fecafea19fd16b4bd
Andrew Fowlie <andrew.j.fowlie@qq.com>
2020-03-26 14:56:50 +1100

    typos in argparse help strs

--- examples/plot_contours.py
+++ examples/plot_contours.py
@@ -15,15 +15,15 @@ import argparse
 
 # Parse user arguments
 
-parser = argparse.ArgumentParser(description='Plot exlcusion limits.')
+parser = argparse.ArgumentParser(description='Plot exclusion limits.')
 parser.add_argument('--z_limit', type=float, help='Exclusion limit, e.g. 1.64', default=1.64)
 
-parser.add_argument('--si', dest='si', action='store_true', help='Do not plot SI scattering cross sections')
-parser.add_argument('--no-si', dest='si', action='store_false', help='Plot SI scattering cross sections')
+parser.add_argument('--si', dest='si', action='store_true', help='Plot SI scattering cross sections')
+parser.add_argument('--no-si', dest='si', action='store_false', help='Do not plot SI scattering cross sections')
 parser.set_defaults(si=True)
 
-parser.add_argument('--sd', dest='sd', action='store_true', help='Do not plot sd scattering cross sections')
-parser.add_argument('--no-sd', dest='sd', action='store_false', help='Plot sd scattering cross sections')
+parser.add_argument('--sd', dest='sd', action='store_true', help='Plot sd scattering cross sections')
+parser.add_argument('--no-sd', dest='sd', action='store_false', help='Do not plot sd scattering cross sections')
 parser.set_defaults(sd=True)
 
 parser.add_argument('experiments', type=str, default=["PICO_60_2017"], nargs='*', help='List of experiments to plot')
